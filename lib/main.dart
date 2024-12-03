79e13a1829d8a9bf1e6c9ef44c23788ee2dddbd7
hejci <tomas.hejhal11@post.cz>
2024-12-03 22:53:22 +0100

    Přidání tlašítek na podstránky

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,10 @@ void main() {
     routes: {
       '/outdoorTraining': (context) => OutdoorTrainingScreen(),
       '/gymTraining': (context) => GymTrainingScreen(),
+      '/PreMadeOutdoorTraining': (context) => PreMadeOutdoorTraining(),
+      '/PreMadeGymTraining': (context) => PreMadeGymTraining(),
+      '/FullListOutdoorTraining': (context) => FullListOutdoorTraining(),
+      '/FullListGymTraining': (context) => FullListGymTraining(),
     },
   ));
 }
@@ -26,7 +30,7 @@ class _MainwidgetState extends State<Mainwidget> {
     return Scaffold(
       body: SafeArea(
         child: Center(
-          child: Column(  // Stacks the text and buttons vertically
+          child: Column( // Stacks the text and buttons vertically
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               // Add text on top
@@ -34,31 +38,29 @@ class _MainwidgetState extends State<Mainwidget> {
                 "Kde budeš trénovat:",
                 style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
               ),
-              SizedBox(height: 16.0),  // Adds space between text and buttons
+              SizedBox(height: 16.0), // Adds space between text and buttons
               Container(
                 padding: EdgeInsets.all(8.0),
                 child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,  // Centers buttons horizontally
+                  mainAxisAlignment: MainAxisAlignment.center, // Centers buttons horizontally
                   children: [
                     Expanded(
                       child: Container(
                         height: 100,
                         child: ElevatedButton(
                           onPressed: () {
-                            // Navigate to the outdoor training screen
                             Navigator.pushNamed(context, '/outdoorTraining');
                           },
                           child: Text("Venku, doma bez přístupu k posilovacím strojům"),
                         ),
                       ),
                     ),
-                    SizedBox(width: 8.0),  // Adds space between buttons
+                    SizedBox(width: 8.0), // Adds space between buttons
                     Expanded(
                       child: Container(
                         height: 100,
                         child: ElevatedButton(
                           onPressed: () {
-                            // Navigate to the gym training screen
                             Navigator.pushNamed(context, '/gymTraining');
                           },
                           child: Text("V posilovně, doma s přístupem k posilovacím strojům"),
@@ -81,31 +83,147 @@ class OutdoorTrainingScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Trénink - vlastní tělo")),
+      appBar: AppBar(title: Text("Trénink venku nebo doma")),
       body: Center(
-        child: Text(
-          "Trénink venku, doma bez přístupu k posilovacím strojům",
-          style: TextStyle(fontSize: 24.0),
-          textAlign: TextAlign.center,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "Trénink venku, doma bez přístupu k posilovacím strojům",
+              style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(height: 16.0),
+            Container(
+              padding: EdgeInsets.all(8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Expanded(
+                    child: Container(
+                      height: 100,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          Navigator.pushNamed(context, '/PreMadeOutdoorTraining');
+                        },
+                        child: Text("Předpřipravené tréninky"),
+                      ),
+                    ),
+                  ),
+                  SizedBox(width: 8.0),
+                  Expanded(
+                    child: Container(
+                      height: 100,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          Navigator.pushNamed(context, '/FullListOutdoorTraining');
+                        },
+                        child: Text("Seznam cviků"),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
   }
 }
 
+
 // Screen for gym/home training with machines
 class GymTrainingScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Trénink - váhy")),
+      appBar: AppBar(title: Text("Trénink v posilovně")),
       body: Center(
-        child: Text(
-          "Trénink v posilovně, doma s přístupem k posilovacím strojům",
-          style: TextStyle(fontSize: 24.0),
-          textAlign: TextAlign.center,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "Trénink v posilovně, doma s přístupem k posilovacím strojům",
+              style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(height: 16.0),
+            Container(
+              padding: EdgeInsets.all(8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Expanded(
+                    child: Container(
+                      height: 100,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          Navigator.pushNamed(context, '/PreMadeGymTraining');
+                        },
+                        child: Text("Předpřipravené tréninky"),
+                      ),
+                    ),
+                  ),
+                   SizedBox(width: 8.0),
+                  Expanded(
+                    child: Container(
+                      height: 100,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          Navigator.pushNamed(context, '/FullListGymTraining');
+                        },
+                        child: Text("Seznam cviků"),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
+    );
+  }
+}
+
+
+// Advanced Outdoor Training Screen
+class PreMadeOutdoorTraining extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Předpřipravené tréninky")),
+    );
+  }
+}
+
+// Advanced Gym Training Screen
+class PreMadeGymTraining extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Předpřipravené tréninky")),
+    );
+  }
+}
+
+// Intermediate Outdoor Training Screen
+class FullListOutdoorTraining extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Seznam cviků")),
+    );
+  }
+}
+
+// Intermediate Gym Training Screen
+class FullListGymTraining extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Seznam cviků")),
     );
   }
 }
